2599939f11bf8809b5f2ef9e889f0b8721b7bc24
杨帆 <yangfanvw@qq.com>
2018-09-15 12:01:16 +0800

    兼容 Swift4.2

---
+++ OCInvokeSwift/OCInvokeSwift/Calculator.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  OCInvokeSwift
+//
+//  Created by teacher on 2017/11/6.
+//  Copyright © 2017年 teacher. All rights reserved.
+//
+
+import Cocoa
+
+class Calculator: NSObject {
+    
+    @objc
+    func add(num1:Int, num2:Int) -> Int {
+        
+        return num1 + num2;
+    }
+
+}
---
+++ OCInvokeSwift/OCInvokeSwift/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  OCInvokeSwift
+//
+//  Created by teacher on 2017/11/6.
+//  Copyright © 2017年 teacher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// 1、swift类必须继承自NSObject 2、新建时创建的桥接文件和Swift调用OC的一样 3、导入的时候不是导入创建的头文件 而是一个命名为“项目名-Swift.h”的头文件 该头文件是自动生成的并且对开发者透明
+#import "OCInvokeSwift-Swift.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        Calculator *cal =  [[Calculator alloc]init];
+        
+        NSInteger result = [cal addWithNum1:20 num2:30];
+        
+        NSLog(@"%ld", (long)result);
+
+        
+        
+    }
+    return 0;
+}
