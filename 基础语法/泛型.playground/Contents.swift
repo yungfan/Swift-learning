e4b7a490e5b8ac93c3a88c22ddd74aac8758c0b0
杨帆 <yangfanvw@qq.com>
2018-11-26 14:56:22 +0800

    更新泛型

--- 基础语法/泛型.playground/Contents.swift
+++ 基础语法/泛型.playground/Contents.swift
@@ -1,59 +1,88 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-
-//针对集合类型，可以在定义类型时指明泛型来限定值的类型
-var array:Array<String> = ["Q", "W", "E"]
-
-var value = array[0]
-
-
-func add(num1:Int, num2:Int) -> Int{
-
-    return num1 + num2
-}
-
-
-func minus<T>(num1:T, num2:T){
+//案例引发的思考
+func swapInt(a: inout Int, b: inout Int){
     
-    print(num1)
-    print(num2)
-
-}
-
-
-
-add(num1:1,num2:2)
-//add(num1:20.2, num2: 20.02)
-
-minus(num1: 20, num2: 20)
-minus(num1: 20.20, num2: 20.02)
-minus(num1: "A", num2: "B")
-
-
-print("-----------------------------")
-
-func swapTwoValue<T>(a: inout T, b: inout T){
-    let tempValue = a
+    let temp = a
     a = b
-    b = tempValue
+    b = temp
+}
+
+var a = 10
+var b = 20
+swapInt(a: &a, b: &b)
+print("a = \(a), b = \(b)")
+
+func swapDouble(a: inout Double, b: inout Double){
+    
+    let temp = a
+    a = b
+    b = temp
+}
+var aa = 10.10
+var bb = 20.20
+swapDouble(a: &aa, b: &bb)
+print("aa = \(aa), bb = \(bb)")
+
+
+
+//泛型
+func swapValues<T>(value1:inout T, value2:inout T){
+    
+    let temp = value1
+    value1 = value2
+    value2 = temp
+}
+
+var oneInt = 10
+var twoInt = 20
+swapValues(value1: &oneInt, value2: &twoInt)
+print("oneInt = \(oneInt), twoInt = \(twoInt)")
+
+
+var oneDouble = 10.1
+var twoDouble = 20.2
+swapValues(value1: &oneDouble, value2: &twoDouble)
+print("oneDouble = \(oneDouble), twoDouble = \(twoDouble)")
+
+var oneString = "hello"
+var twoString = "world"
+swapValues(value1: &oneString, value2: &twoString)
+print("oneString = \(oneString), twoString = \(twoString)")
+
+
+//类型关联
+protocol SomeProtocol {
+    
+    associatedtype T:UIView
+    
+    func method1(argv : T)
+    func method2(argv : T)
+    
 }
 
 
-var oneInt = 22;
-var twoInt = 33;
+struct A : SomeProtocol{
+   
+    func method1(argv: UIButton) {
+        
+    }
+    
+    
+    func method2(argv: UIButton) {
+        
+    }
+}
 
-swapTwoValue(a:&oneInt, b: &twoInt)
-print("oneInt:\(oneInt),twoInt:\(twoInt)")
 
-var oneStr = "hello"
-var twoStr = "world"
-
-swapTwoValue(a:&oneStr, b: &twoStr)
-print("oneStr:\(oneStr),twoStr:\(twoStr)")
-
-var oneDouble = 10.01
-var twoDouble = 20.02
-swapTwoValue(a:&oneDouble, b: &twoDouble)
-print("oneDouble:\(oneDouble),twoDouble:\(twoDouble)")
+class B : SomeProtocol {
+    
+    func method1(argv: UIImageView) {
+        
+    }
+    
+    func method2(argv: UIImageView) {
+        
+    }
+  
+}
